2af7f5a451d7bf2d0e3b87e9876afa8c393d7d3b
suyash <suyashambule1234@gmail.com>
2025-09-20 19:59:49 +0530

    Data validation Completed

--- src/IPL_Predictor/config/configuration.py
+++ src/IPL_Predictor/config/configuration.py
@@ -3,7 +3,7 @@ from src.IPL_Predictor.utils.common import read_yaml, create_directories
 
 from src.IPL_Predictor.entity.config_entity import (DataIngestionConfig,)
 
-class ConfigurationManager:
+class ConfigurationManager_DataIngestion:
     def __init__(self,
                  config_filepath=CONFIG_FILE_PATH,
                  params_filepath = PARAMS_FILE_PATH,
@@ -27,3 +27,19 @@ class ConfigurationManager:
 
         )
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+            unzip_data_dir_1 = config.unzip_data_dir_1
+        )
+
+        return data_validation_config
--- src/IPL_Predictor/entity/config_entity.py
+++ src/IPL_Predictor/entity/config_entity.py
@@ -8,3 +8,11 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass
+class DataValidationConfig:
+    root_dir:Path
+    STATUS_FILE:str
+    unzip_data_dir:Path
+    all_schema:dict
+    unzip_data_dir_1:Path
